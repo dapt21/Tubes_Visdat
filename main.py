a42142871e56d87f48cc32ec789a942206c42053
dapt21 <davidharmawansaputra@gmail.com>
2024-12-26 18:06:25 +0700

    add World Plot

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
 
+data = pd.read_csv('dataCovid.csv')
+
+st.title("Kasus Covid di Seluruh Dunia")
+fig = px.choropleth(
+    data, 
+    locationmode="country names", 
+    locations="Country/Region", 
+    color="TotalDeaths",
+    hover_name="Country/Region",
+    hover_data="TotalDeaths",
+    color_continuous_scale=px.colors.sequential.Plasma,
+)
+st.plotly_chart(fig)
